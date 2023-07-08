b2fa01eaa0d74acf1bac41ea559b3024a0639c97
Aleecia Everett <aleeciarose@gmail.com>
2023-07-07 22:10:24 -0400

    Completed Section 5 Exercise
    Completed Section 5 Exercise, including creating first test class, and first app class, and getting first unit test to pass

---
+++ 05-create-unit-tests/projects/starter/app/src/main/java/com/raywenderlich/android/cocktails/game/model/Game.kt
@@ -0,0 +1,10 @@
+package com.raywenderlich.android.cocktails.game.model;
+
+public class Game {
+    var currentScore = 0
+        private set
+
+    fun incrementScore(){
+        currentScore++
+    }
+}
