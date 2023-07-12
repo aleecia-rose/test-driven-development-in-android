7a83cf6c26c2cf021e4e3bd6818b9cfa52fa0114
Aleecia Everett <aleeciarose@gmail.com>
2023-07-12 17:13:15 -0400

    Completed Section 12 Exercise
    Completed Section 12 Exercise, including creating an integration test for DetailViewModelTest, verifying that new items are saved to the database.

--- 12-create-your-first-integration-test/projects/starter/app/src/main/java/com/raywenderlich/android/wishlist/DetailViewModel.kt
+++ 12-create-your-first-integration-test/projects/starter/app/src/main/java/com/raywenderlich/android/wishlist/DetailViewModel.kt
@@ -38,6 +38,7 @@ import com.raywenderlich.android.wishlist.persistance.Repository
 class DetailViewModel(private val repository: Repository) : ViewModel() {
 
   fun saveNewItem(wishlist: Wishlist, name: String) {
+    repository.saveWishlistItem(Wishlist("", listOf()))
   }
 
   fun getWishlist(id: Int): LiveData<Wishlist> {
