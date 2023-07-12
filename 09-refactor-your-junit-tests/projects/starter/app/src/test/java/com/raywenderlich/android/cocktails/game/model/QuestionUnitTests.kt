139720cd9a182418354bdbc97da665bf4ae858e4
Aleecia Everett <aleeciarose@gmail.com>
2023-07-12 00:18:36 -0400

    Completed Section 9 Exercise
    Completed Section 9 Exercise, including refactoring QuestionUnitTests file.

--- 09-refactor-your-junit-tests/projects/starter/app/src/test/java/com/raywenderlich/android/cocktails/game/model/QuestionUnitTests.kt
+++ 09-refactor-your-junit-tests/projects/starter/app/src/test/java/com/raywenderlich/android/cocktails/game/model/QuestionUnitTests.kt
@@ -1,32 +1,35 @@
 package com.raywenderlich.android.cocktails.game.model
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class QuestionUnitTests {
+
+    private lateinit var question : Question
+    @Before
+    fun setup(){
+        question = Question("CORRECT", "INCORRECT")
+    }
     @Test
     fun whenCreatingQuestion_shouldNotHaveAnsweredOption(){
-        val question = Question("CORRECT", "INCORRECT")
         Assert.assertNull(question.answeredOption)
     }
 
     @Test
     fun whenAnswering_shouldHaveAnsweredOption(){
-        val question = Question("CORRECT", "INCORRECT")
         question.answer("INCORRECT")
         Assert.assertEquals("INCORRECT", question.answeredOption)
     }
 
     @Test
     fun whenAnswering_withCorrectOption_shouldReturnTrue(){
-        val question = Question("CORRECT", "INCORRECT")
         val result = question.answer("CORRECT")
         Assert.assertTrue(result)
     }
 
     @Test
     fun whenAnswering_withInCorrectOption_shouldReturnTrue(){
-        val question = Question("CORRECT", "INCORRECT")
         val result = question.answer("CORRECT")
         Assert.assertTrue(result)
     }
